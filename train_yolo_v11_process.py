53f52135d09a996d508777b4b4708524cdbe3a1a
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-18 14:30:50 +0100

    chore: Qt 6

--- train_yolo_v11_process.py
+++ train_yolo_v11_process.py
@@ -186,8 +186,8 @@ class TrainYoloV11Factory(dataprocess.CTaskFactory):
         self.info.short_description = "Train YOLOv11 object detection models."
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Detection"
-        self.info.version = "1.1.1"
-        self.info.min_ikomia_version = "0.15.0"
+        self.info.version = "1.2.0"
+        self.info.min_ikomia_version = "0.16.0"
         self.info.icon_path = "images/icon.png"
         self.info.authors = "Jocher, G., Chaurasia, A., & Qiu, J"
         self.info.article = "YOLO by Ultralytics"
